f9e34fbf9b14efa3b5ab34fdcdbf6e3aadd19cb8
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-06-14 22:29:33 -0300

    Adicionei anotações sobre event binding

---
+++ crud/frontend/src/app/components/template/teste.ts
@@ -0,0 +1,11 @@
+@Component({
+    selector: 'app-product-create',
+    templateUrl: './product-create.compnent.html',
+    styleUrl: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+    createProduct() {
+        //...
+    }
+}
