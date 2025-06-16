d882c76c5c071b7ed40fe9b1342ef5f6038ca35a
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-06-15 22:34:42 -0300

    Anotei sobre o Angular Routes

--- crud/frontend/src/app/components/template/teste.ts
+++
@@ -1,8 +0,0 @@
-@Component({
-    selector: 'app-product-create',
-    templateUrl: './product-create.compnent.html',
-    styleUrl: ['./product-create.component.css']
-})
-export class ProductCreateComponent implements OnInit {
-    nome: string;       
-}
