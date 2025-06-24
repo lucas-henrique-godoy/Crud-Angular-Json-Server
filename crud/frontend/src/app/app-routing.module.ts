15f693227c5c490635cb730c448cffcbfc659ba0
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-06-23 21:56:48 -0300

    Criei o componente de cadastro de produtos e registrei as rotas de home.component e product.-crud.compoennt

--- crud/frontend/src/app/app-routing.module.ts
+++ crud/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
-const routes: Routes = [];
+const routes: Routes = [  //Registrando as rotas dos componentes principais
+{
+  path: "",
+  component: HomeComponent
+}, 
+{
+  path: "/product",
+  component: ProductCrudComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
