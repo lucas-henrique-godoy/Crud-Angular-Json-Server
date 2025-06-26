8215ac37b4a82edbf4a95fd2e7d647de77cae8fd
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-06-25 22:15:23 -0300

    Treinando diretivas estruturais.

--- crud/frontend/src/app/directives/for.directive.ts
+++ crud/frontend/src/app/directives/for.directive.ts
@@ -1,15 +1,24 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, OnInit, Input, ViewContainerRef, TemplateRef } from '@angular/core';
 
 @Directive({
   selector: '[myFor]'
 })
 export class ForDirective implements OnInit{
 
-  constructor() {
+  @Input('myForEm') numbers: number[]
+ 
+
+  constructor(
+    private container: ViewContainerRef,
+    private template: TemplateRef<any>) {
     
    }
   
    ngOnInit(): void {
+     for(let number of this.numbers){
+        this.container.createEmbeddedView(this.template, { $implicit : number})
+     }
+    
      
    }
 }
